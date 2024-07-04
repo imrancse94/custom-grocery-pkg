e277e5494e4f4542012fc059b70f4326bdbe0eca
Imran Hossain <imrancse94@gmail.com>
2024-07-04 13:14:21 +0600

    Initial Commit

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" integrity="sha512-PgQMlq+nqFLV4ylk1gwUOgm6CtIIXkKwaIHp/PAIWHzig/lKZSEGKEysh0TCVbHJXCLN7WetD8TFecIky75ZfQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-    @vite('packages/Grocery/src/resources/js/grocery/app.js')
+    @vite('resources/js/grocery/app.js')
 </head>
 <body>
 <div id="app"></div>
